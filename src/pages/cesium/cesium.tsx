26148c6a7ac6f841dd1ad5e6f82204cd51952d7f
lutianwen <1244832273@qq.com>
2021-09-23 11:06:29 +0800

    feat: cesium报错

---
+++ src/pages/cesium/cesium.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: 鲁田文
+ * @Date: 2021-09-23 09:26:10
+ * @LastEditTime: 2021-09-23 09:34:48
+ * @LastEditors: 鲁田文
+ * @Description:
+ */
+import React from 'react';
+import { Viewer } from 'resium';
+
+const CesiumGlobal = () => {
+  return <Viewer></Viewer>;
+};
+
+export default CesiumGlobal;
--- src/router/routers.tsx
+++ src/router/routers.tsx
@@ -1,113 +1,110 @@
 /*
  * @Author: 鲁田文
  * @Date: 2021-03-31 14:22:13
- * @LastEditTime: 2021-06-03 17:53:15
+ * @LastEditTime: 2021-09-23 11:03:50
  * @LastEditors: 鲁田文
  * @Description:
  */
-import { format } from "path";
-import { lazy } from "react";
+import { format } from 'path';
+import { lazy } from 'react';
 
-import { RoutesOption } from "./index";
-import Layout from "@/components/layout";
+import { RoutesOption } from './index';
+import Layout from '@/components/layout';
 
-const Login = lazy(() => import("@/pages/login"));
-const Home = lazy(() => import("@/pages/home"));
-const Auth = lazy(() => import("@/pages/auth"));
-const Auth1 = lazy(() => import("@/pages/auth/index copy"));
-const Auth2 = lazy(() => import("@/pages/auth/index copy 2"));
+const Login = lazy(() => import('@/pages/login'));
+const Home = lazy(() => import('@/pages/home'));
+const Auth = lazy(() => import('@/pages/auth'));
+const Auth1 = lazy(() => import('@/pages/auth/index copy'));
+const Auth2 = lazy(() => import('@/pages/auth/index copy 2'));
+
+const CesiumGlobal = lazy(() => import('@/pages/cesium/cesium'));
 
 const permissions = ['admin'];
 
 const routes: RoutesOption[] = [
   {
-    path: "/login",
+    path: '/login',
     component: Login,
-    title: "登录",
+    title: '登录',
   },
   {
-    path: "/404",
+    path: '/404',
     component: () => <div>404</div>,
-    title: "404",
+    title: '404',
   },
   {
-    path: "/manage",
+    path: '/cesium',
+    component: CesiumGlobal,
+    title: '地图',
+  },
+  {
+    path: '/manage',
     component: Layout,
     meta: {
       menu: true,
     },
     children: [
       {
-        path: "/home",
+        path: '/home',
         component: Home,
-        title: "首页",
+        title: '首页',
         meta: {
           menu: true,
-          permissions: [
-            ...permissions,
-          ]
+          permissions: [...permissions],
         },
       },
       {
-        path: "/home1",
+        path: '/home1',
         component: Auth1,
-        title: "首页1",
+        title: '首页1',
         meta: {
           menu: true,
         },
       },
       {
-        path: "/home2",
+        path: '/home2',
         component: Auth2,
-        title: "首页2",
+        title: '首页2',
         meta: {
           menu: true,
         },
       },
       {
-        path: "/auth",
-        title: "活动主体",
+        path: '/auth',
+        title: '活动主体',
         meta: {
           menu: true,
-          permissions: [
-            ...permissions,
-          ]
+          permissions: [...permissions],
         },
         children: [
           {
-            path: "/activity",
+            path: '/activity',
             component: Auth,
-            title: "活动",
+            title: '活动',
             meta: {
               menu: true,
-              permissions: [
-                ...permissions,
-              ]
+              permissions: [...permissions],
             },
           },
           {
-            path: "/activity1",
+            path: '/activity1',
             component: Auth1,
-            title: "活动1",
+            title: '活动1',
             meta: {
               menu: true,
-              permissions: [
-                ...permissions,
-              ]
+              permissions: [...permissions],
             },
           },
           {
-            path: "/activity2",
+            path: '/activity2',
             component: Auth2,
-            title: "活动2",
+            title: '活动2',
             meta: {
               menu: true,
-              permissions: [
-                'user1',
-              ]
+              permissions: ['user1'],
             },
           },
-        ]
+        ],
       },
     ],
   },
